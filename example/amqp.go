f46d17df722464e11f0e2d693a757b630d255b94
fanjindong <fanjindong@100.me>
2021-08-02 11:00:10 +0800

    feat: add amqp example

---
+++ example/amqp.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"github.com/fanjindong/go-pools"
+	"github.com/streadway/amqp"
+	"time"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://root:password@127.0.0.1:5672/")
+	if err != nil {
+		panic("Failed to connect to RabbitMQ")
+	}
+	factory := func(context.Context) (pools.Resource, error) {
+		return conn.Channel()
+	}
+	rp := pools.NewResourcePool(factory, 10, 30, 1*time.Hour, 1, nil)
+	defer rp.Close()
+
+	resource, err := rp.Get(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	channel := resource.(*amqp.Channel)
+	_ = channel.Publish("exchange", "routingKey", false, false, amqp.Publishing{
+		Headers:         amqp.Table{},
+		ContentType:     "text/plain",
+		ContentEncoding: "",
+		Body:            []byte("body"),
+		DeliveryMode:    amqp.Transient,
+		Priority:        0,
+	})
+	rp.Put(channel)
+}
---
+++ example/go.mod
@@ -0,0 +1,8 @@
+module github.com/fanjindong/go-pools/example
+
+go 1.15
+
+require (
+	github.com/fanjindong/go-pools v0.0.0-20210731100507-82fdb4eb9ece // indirect
+	github.com/streadway/amqp v1.0.0
+)
